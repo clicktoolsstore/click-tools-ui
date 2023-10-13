0b2d8c5733d25ab945e5ab9c51945e7451c85d18
Luiz Bueno <luiz504@hotmail.com>
2023-10-13 17:05:45 -0300

    test: unity testing typography component

--- packages/docs/src/stories/components/typography/Text.stories.tsx
+++ packages/docs/src/stories/components/typography/Text.stories.tsx
@@ -2,7 +2,9 @@ import { Meta, StoryObj } from '@storybook/react'
 import { Text, TextProps } from '@click-tools-ui/react'
 
 const sizeOptions: TextProps['size'][] = ['xs', 'md', '2xl']
-
+const weightOptions: TextProps['weight'][] = ['regular', 'medium', 'bold']
+const asOptions: TextProps['as'][] = ['span', 'label', 'p', 'div']
+const leadingOptions: TextProps['leading'][] = ['1.3', '1.6']
 /** This component uses the `textStyle` preset. */
 const meta: Meta<TextProps> = {
   title: 'Typography/Components/Text',
@@ -12,6 +14,8 @@ const meta: Meta<TextProps> = {
     children: 'Some Text',
     size: 'md',
     as: 'span',
+    weight: 'regular',
+    leading: '1.3',
   },
   argTypes: {
     size: {
@@ -24,9 +28,27 @@ const meta: Meta<TextProps> = {
         },
       },
     },
+    weight: {
+      control: 'inline-radio',
+      options: weightOptions,
+      table: {
+        defaultValue: {
+          summary: 'regular',
+        },
+      },
+    },
+    leading: {
+      control: 'inline-radio',
+      options: leadingOptions,
+      table: {
+        defaultValue: {
+          summary: '1.3',
+        },
+      },
+    },
     as: {
       control: 'inline-radio',
-      options: ['span', 'label', 'p', 'div'] as TextProps['as'][],
+      options: asOptions,
       description: 'span/label: inline  - p/div: block',
       table: {
         defaultValue: {
--- packages/react/CHANGELOG.md
+++ packages/react/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @click-tools-ui/react
 
+## 0.4.3
+
+### Patch Changes
+
+- Testing typography components
+
 ## 0.4.2
 
 ### Patch Changes
